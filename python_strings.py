6471137b5419b94550c8b13bd00289c97df2163a
jasonfarfan <jasonfarfan@mail.weber.edu>
2023-02-01 18:24:38 -0700

    finish

--- python_strings.py
+++ python_strings.py
@@ -3,11 +3,15 @@
 #   - my_first_name
 #       -set this equal to your first name
 #   - my_last_name
+my_first_name = 'Jason'
+my_last_name = 'farfan'
 #       -set this equal to your last name
 #   - my_year_of_birth
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
+my_year_of_birth = 2003
 #   - current_year
 #       -set this equal to 2020
+current_year = 2023
 
 
 
@@ -20,8 +24,10 @@
 #       - second letter of your last name (use the -index)
 #       - first two letter of your first name (use the +index)
 #       - second two letter of your last name (use the -index)
-
-
+print(my_first_name)
+print(my_last_name)
+print(my_first_name[0])
+print(my_last_name[-1])
 
 
 #TODO Combining Strings
@@ -29,7 +35,8 @@
 #       -first name and last name combined
 #       -first name six times
 
-
+print( my_first_name + '' + my_last_name)
+print(my_first_name*6)
 
 
 
@@ -38,16 +45,20 @@
 #       - first name last name -was born in- year of birth
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
 
-
+print(my_first_name, my_last_name, 'was born in', my_year_of_birth)
+print(my_first_name, my_last_name, 'was born in', my_year_of_birth, '.', my_first_name, 'enjoyed celebrating', current_year)
 
 # TODO Escape characters
 #   - Print the following items (one per line) (print using variables)
 #       - possesive first name -birth year is- year of birth 
 #       - tab last name current year
-
-
+print(my_first_name + '\'s ' + "birth year is", + my_year_of_birth)
+print('\t', my_last_name, current_year)
 # TODO String methods
 #   - Print the following items (one per line) (print using variables)
 #       - first name and last name in lower case
 #       - length of last name
 #       - first name and last name all in upper case
+print(my_first_name.casefold(), my_last_name.casefold())
+print(len(my_last_name))
+print(my_first_name.upper(), my_last_name.upper())
